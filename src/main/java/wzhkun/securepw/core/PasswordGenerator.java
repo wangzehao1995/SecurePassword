8f27b3fdb8d8947c146a25be33dd8b62d1ab598a
wangzehao <wangzehao@WANGZEHAO-DT>
2015-07-22 12:58:07 +0800

    debug

--- src/main/java/wzhkun/securepw/core/PasswordGenerator.java
+++ src/main/java/wzhkun/securepw/core/PasswordGenerator.java
@@ -2,63 +2,80 @@ package wzhkun.securepw.core;
 
 import java.io.Serializable;
 
-public class PasswordGenerator implements Serializable{
+public class PasswordGenerator implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	private int 长度=12;
-	private boolean 大小写混合=true;
-	private boolean 字母数字混合=true;
-	private boolean 字母符号混合=true;
-	private boolean 第一位为字母=true;
-	private boolean 至少一位为数字=true;
-	private boolean 至少一位为特殊符号=true;
-	
-	private static final String 小写字母="abcdefghijklmnopqrstuvwxyz";
-	private static final String 大写字母="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	private static final String 数字="0123456789";
-	private static final String 特殊符号="~!@#$%^&*()_+-={}|[]\\:;<>?,./";
-	
-	public PasswordGenerator(){
-		
+
+	private int 长度 = 12;
+	private boolean 大小写混合 = true;
+	private boolean 字母数字混合 = true;
+	private boolean 字母符号混合 = true;
+	private boolean 第一位为字母 = true;
+	private boolean 至少一位为数字 = true;
+	private boolean 至少一位为特殊符号 = true;
+
+	private static final String 小写字母 = "abcdefghijklmnopqrstuvwxyz";
+	private static final String 大写字母 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	private static final String 数字 = "0123456789";
+	private static final String 特殊符号 = "~!@#$%^&*()_+-={}|[]\\:;<>?,./";
+
+	public PasswordGenerator() {
+
 	}
-	
-	public String generate(){
-		String 库=小写字母;
-		if(大小写混合){
-			库+=大写字母;
+
+	public String generate() {
+		String 库 = 小写字母;
+		if (大小写混合) {
+			库 += 大写字母;
 		}
-		if(字母数字混合){
-			库+=数字;
+		if (字母数字混合) {
+			库 += 数字;
 		}
-		if(字母符号混合){
-			库+=特殊符号;
+		if (字母符号混合) {
+			库 += 特殊符号;
 		}
-		
-		String 密码="";
-		for(int i=0;i<长度;i++){
-			密码+=random(库);
+
+		String 密码 = "";
+		for (int i = 0; i < 长度; i++) {
+			密码 += random(库);
 		}
-		
-		if(第一位为字母){
-			密码=random(小写字母)+密码.substring(1);
+		if (至少一位为数字 && 至少一位为特殊符号) {
+			while(!(contains(密码, 数字)&&contains(密码, 特殊符号))){
+				密码 = randomReplace(密码, 数字);
+				密码 = randomReplace(密码, 特殊符号);
+			}
+		} else if (至少一位为数字) {
+			while (!contains(密码, 数字)) {
+				密码 = randomReplace(密码, 数字);
+			}
+		} else if (至少一位为特殊符号) {
+			while (!contains(密码, 特殊符号)) {
+				密码 = randomReplace(密码, 特殊符号);
+			}
 		}
-		if(至少一位为数字){
-			密码=randomReplace(密码, 数字);
+		if (第一位为字母) {
+			密码 = random(小写字母) + 密码.substring(1);
 		}
-		if(至少一位为特殊符号){
-			密码=randomReplace(密码, 特殊符号);
-		}
+
 		return 密码;
 	}
-	
-	private String randomReplace(String object,String library){
-		char[] chars=object.toCharArray();
-		chars[(int) (Math.random()*chars.length)]=random(library);
+
+	private String randomReplace(String object, String library) {
+		char[] chars = object.toCharArray();
+		chars[(int) (Math.random() * chars.length)] = random(library);
 		return new String(chars);
 	}
-	
-	private char random(String library){
-		return library.charAt((int) (Math.random()*library.length()));
+
+	private char random(String library) {
+		return library.charAt((int) (Math.random() * library.length()));
+	}
+
+	private boolean contains(String source, String lib) {
+		for (char c : lib.toCharArray()) {
+			if (source.contains(Character.toString(c))) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public int get长度() {
